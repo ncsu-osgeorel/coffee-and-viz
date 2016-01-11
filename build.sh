8793d0e9d8fcf9a4d3226eb449ed3a51722cb50f
baharmon <baharmon@ncsu.edu>
2016-01-11 14:08:18 -0500

    major updates, removed header and footers, etc

---
+++ build.sh
@@ -0,0 +1,8 @@
+./copy-common-files.py --dst-dir=build
+
+./build-slides.py --outdir=build \
+    --title="Coffee &amp; Viz" \
+    --meta-description="Serious gaming with Tangible Landscape" \
+    --meta-author="Brendan Harmon and Anna Petrasova" \
+    --outfile=hunt.html \
+    tangible_landscape.html
