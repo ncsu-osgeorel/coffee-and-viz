5eff74595fe2d43f196e1e16bd1803e736d4d562
baharmon <baharmon@ncsu.edu>
2016-01-11 14:44:56 -0500

    edited title page

--- build.sh
+++ build.sh
@@ -5,4 +5,13 @@
     --meta-description="Serious gaming with Tangible Landscape" \
     --meta-author="Brendan Harmon and Anna Petrasova" \
     --outfile=hunt.html \
-    tangible_landscape.html
+    tangible_landscape.html invasive_species.html coastal_games.html
+
+./build-slides.py --outdir=build \
+    --title="Coffee &amp; Viz" \
+    --meta-description="Serious gaming with Tangible Landscape" \
+    --meta-author="Brendan Harmon and Anna Petrasova" \
+    --head="head_original.html" \
+    --foot="foot_original.html" \
+    --outfile=index.html \
+    tangible_landscape.html invasive_species.html coastal_games.html
