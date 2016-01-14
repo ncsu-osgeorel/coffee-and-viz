00f167a0106f6396cfa1897389b74da8611882ab
Brendan <baharmon@gmail.com>
2016-01-14 10:54:43 -0500

    updated build order

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
     --meta-description="Serious gaming with Tangible Landscape" \
     --meta-author="Brendan Harmon and Anna Petrasova" \
     --outfile=hunt.html \
-    tangible_landscape.html invasive_species.html coastal_games.html
+    complex_problems.html tangible_landscape.html invasive_species.html coastal_games.html
 
 #./build-slides.py --outdir=build \
 #    --title="Coffee &amp; Viz" \
@@ -16,9 +16,9 @@
 #    --outfile=index.html \
 #    tangible_landscape.html invasive_species.html coastal_games.html
 
-./build-slides.py --outdir=build \
-    --title="Coffee &amp; Viz: Complex environmental problems require bigger data and better models" \
-    --meta-description="Complex environmental problems require bigger data and better models" \
-    --meta-author="" \
-    --outfile=complex_problems.html \
-    complex_problems.html
+#./build-slides.py --outdir=build \
+#    --title="Coffee &amp; Viz: Complex environmental problems require bigger data and better models" \
+#    --meta-description="Complex environmental problems require bigger data and better models" \
+#    --meta-author="" \
+#    --outfile=complex_problems.html \
+#    complex_problems.html
