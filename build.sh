b05dae6deae8bf250096832b873d56e2e9788b4f
Vaclav Petras <wenzeslaus@gmail.com>
2016-01-13 23:22:04 -0500

    port of Jelena's slides

--- build.sh
+++ build.sh
@@ -15,3 +15,10 @@
 #    --foot="foot_original.html" \
 #    --outfile=index.html \
 #    tangible_landscape.html invasive_species.html coastal_games.html
+
+./build-slides.py --outdir=build \
+    --title="Coffee &amp; Viz: Complex environmental problems require bigger data and better models" \
+    --meta-description="Complex environmental problems require bigger data and better models" \
+    --meta-author="" \
+    --outfile=complex_problems.html \
+    complex_problems.html
